fa627826d1312d86aae414536cf12a6578f11069
Yashrao-codes <r_yashaswi@yahoo.co.in>
2024-08-15 02:11:13 +0530

    Delete public directory

--- public/service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', event => {
-  console.log('Service worker installing...');
-  // Cache necessary assets
-});
-
-self.addEventListener('fetch', event => {
-  console.log('Fetching:', event.request.url);
-  // Serve assets from cache
-});
